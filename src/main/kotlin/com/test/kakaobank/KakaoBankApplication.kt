5cce0013144611bc8374fd4123620749dcf99556
imjoo <ganji1126@gmai.com>
2023-03-18 13:17:10 +0900

    초기 세팅

---
+++ src/main/kotlin/com/test/kakaobank/KakaoBankApplication.kt
@@ -0,0 +1,11 @@
+package com.test.kakaobank
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KakaoBankApplication
+
+fun main(args: Array<String>) {
+    runApplication<KakaoBankApplication>(*args)
+}
